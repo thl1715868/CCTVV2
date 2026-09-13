cf52facdd2f7169b36f54ae9f7482c63b558c0fb
thl1715868 <thl1715868@uoi.gr>
2026-09-13 17:08:52 +0300

    Update CCTV_ONLINE_API_URL to new endpoint

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.CCTV_ONLINE_API_URL = "https://script.google.com/macros/s/AKfycbzd5gtvbQn553nPAStdUWmVmviBG0QCwYsRVfA8l3drRuz9hvqIIla5SqlJk3hbEQVo/exec";
+window.CCTV_ONLINE_API_URL = "https://script.google.com/macros/s/AKfycbxuU6nKYscL7gnG3pnUoVsh4a1ewVEyKCjS8vPeHuw5bgbuV3JurpzrbkSvx6rHH-XO/exec";
